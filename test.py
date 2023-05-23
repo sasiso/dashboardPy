d039331558515ab3acaa610a67e4e0ca03509c5a
satbir.singh@planetinnovation.com.au <satbir.singh@planetinnovation.com.au>
2023-05-23 12:25:03 +1000

    added buttons

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLineEdit, QSizePolicy
-from PyQt5.QtCore import Qt, pyqtSignal
+import time
+import uuid
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLineEdit, QSizePolicy, QPushButton
+from PyQt5.QtCore import Qt, pyqtSignal, QTimer
 from PyQt5.QtGui import QWheelEvent
 
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Dashboard v0.0.3")
+        self.setWindowTitle("Dashboard v0.0.4")
         self.setGeometry(100, 100, 400, 300)
 
         # Create a central widget and set it as the main window's central widget
@@ -30,7 +32,7 @@ class MyWindow(QMainWindow):
         # Create the tasks table if it doesn't exist
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS tasks (
-                id INTEGER PRIMARY KEY,
+                uid TEXT PRIMARY KEY,
                 task_name TEXT,
                 color TEXT
             )
@@ -38,6 +40,60 @@ class MyWindow(QMainWindow):
 
         # Read tasks from the database and create text input fields
         self.load_tasks()
+
+        # Create countdown button
+        self.countdown_button = QPushButton("60")
+        self.countdown_button.clicked.connect(self.start_countdown)
+        self.layout.addWidget(self.countdown_button, self.field_row, self.field_col)
+
+        # Create count-up button
+        self.countup_button = QPushButton("5")
+        self.countup_button.clicked.connect(self.start_countup)
+        self.layout.addWidget(self.countup_button, self.field_row, self.field_col + 1)
+
+        # Create counters
+        self.countdown_counter = 60
+        self.countup_counter = 1
+
+        # Create timers
+        self.countdown_timer = QTimer()
+        self.countdown_timer.timeout.connect(self.update_countdown)
+        self.countup_timer = QTimer()
+        self.countup_timer.timeout.connect(self.update_countup)
+
+        # Start the timers
+        self.countdown_timer.start(1000*60)
+        self.countup_timer.start(1000*60)
+
+    def start_countdown(self):
+        if self.countdown_timer.isActive():
+            self.countdown_timer.stop()
+        else:
+            self.countdown_timer.start(1000*60)
+
+    def start_countup(self):
+        if self.countup_timer.isActive():
+            self.countup_timer.stop()
+        else:
+            self.countup_timer.start(1000*60)
+
+    def update_countdown(self):
+        self.countdown_counter -= 1
+        self.countdown_button.setText(str(self.countdown_counter))
+
+        if self.countdown_counter < 0:
+            self.countdown_button.setStyleSheet("background-color: red;")
+        else:
+            self.countdown_button.setStyleSheet("background-color: green;")
+
+    def update_countup(self):
+        if self.countup_counter <= 5:
+            self.countup_button.setStyleSheet("background-color: green;")
+        else:
+            self.countup_button.setStyleSheet("background-color: red;")
+
+        self.countup_button.setText(str(self.countup_counter))
+        self.countup_counter += 1
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -64,21 +120,29 @@ class MyWindow(QMainWindow):
 
     def save_text_input(self):
         sender = self.sender()
-        if isinstance(sender, QLineEdit):
+        if isinstance(sender, CustomLineEdit):
             task_name = sender.text()
             color = sender.color
-            self.cursor.execute("INSERT INTO tasks (task_name, color) VALUES (?, ?)", (task_name, color))
+            uid = sender.uid
+            if uid:
+                self.cursor.execute("UPDATE tasks SET task_name=?, color=? WHERE uid=?", (task_name, color, uid))
+            else:
+                uid = str(uuid.uuid4())
+                self.cursor.execute("INSERT INTO tasks (uid, task_name, color) VALUES (?, ?, ?)", (uid, task_name, color))
+                sender.uid = uid
             self.conn.commit()
 
     def load_tasks(self):
-        self.cursor.execute("SELECT task_name, color FROM tasks")
+        self.cursor.execute("SELECT uid, task_name, color FROM tasks")
         tasks = self.cursor.fetchall()
         for task in tasks:
-            task_name = task[0]
-            color = task[1]
+            uid = task[0]
+            task_name = task[1]
+            color = task[2]
             self.add_text_input_field(task_name)
             last_widget = self.layout.itemAt(self.layout.count() - 1).widget()
             last_widget.set_color(color)
+            last_widget.uid = uid
 
 
 class CustomLineEdit(QLineEdit):
@@ -87,6 +151,7 @@ class CustomLineEdit(QLineEdit):
     def __init__(self):
         super().__init__()
         self.color = "white"
+        self.uid = ""
 
     def wheelEvent(self, event: QWheelEvent):
         colors = ["green", "yellow", "red", "white"]
